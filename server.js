4914af3b44a16bf4143312461ef9389ea73708d9
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-09-27 16:08:41 +0200

    implement post route handler for /recipes endpoint

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post('/shopping-list', jsonParser, (req, res)=> {
   const requiredFields = ['name', 'budget'];
   for (let i=0; i<requiredFields.length; i++) {
     if (!requiredFields[i] in req.body) {
-      const message = `Missing ${field} in request body.`;
+      const message = `Missing ${requiredFields[i]} in request body.`;
       console.error(message);
       return res.status(400).send(message);
     }
@@ -38,6 +38,20 @@ Recipes.create('pancakes', ['flower', 'milk', 'water', 'eggs', 'milkyway']);
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
+});
+
+app.post('/recipes', jsonParser, (req, res)=> {
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    if (!requiredFields[i] in req.body) {
+      const message = `Missing ${requiredFields[i]} in request body.`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  return res.status(201).json(item);
 });
 
 // start the process at an envirnoment or 8080 port
