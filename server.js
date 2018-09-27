cb63a783273b7dc0d7a63c02ce33e2297303d48b
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-09-27 15:04:22 +0200

    add a couple of items to the shoppinglist model and send it in json format to the user on get request

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const app = express();
-
 const morgan = require('morgan'); // use morgan for logging the events on the HTTP layer
 const bodyParser = require('body-parser'); // use for parsing JSON data sent in HTTP requests
+const {ShoppingList} = require('./models'); // import the shopping list model
+
+const jsonParser = bodyParser.json();
+const app = express();
 
 app.use(morgan('common'));
 
-app.get('/', (req, res) => {
-  res.send('Hey. I received an HTTP get request!');
+//add some items to ShoppingList. This is normally done on the database side
+
+ShoppingList.create('beans', 2);
+ShoppingList.create('tomatoes', 3);
+ShoppingList.create('peppers', 4);
+
+app.get('/shopping-list', (req, res) => {
+  res.json(ShoppingList.get());
 });
 
 // start the process at an envirnoment or 8080 port
