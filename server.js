8bbae1c4ebf58139555bd0f3f3b83206cd7ac452
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-09-27 11:46:46 +0200

    add boilerplate node code

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan'); // use morgan for logging the events on the HTTP layer
+const bodyParser = require('body-parser'); // use for parsing JSON data sent as requests
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('Hey!');
+});
+
+// start the process at an envirnoment or 8080 port
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`The process has started listening at port ${process.env.PORT || 8080}`);
+}),
+
