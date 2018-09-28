f85ffa03cba77cd1a8e3820fef04adabbb951975
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-09-28 09:55:39 +0200

    add DELETE route handlers for shopping-list and recipes

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-//add some items to ShoppingList. This is normally done on the database side
+// add some items to ShoppingList. This is normally done on the database side
 
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
-ShoppingList.create('peppers', 4);
+
+// retrieve items from the shopping list
 
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
+
+// add items to the shopping list
 
 app.post('/shopping-list', jsonParser, (req, res)=> {
   const requiredFields = ['name', 'budget'];
@@ -33,12 +36,26 @@ app.post('/shopping-list', jsonParser, (req, res)=> {
   return res.status(201).json(item);
 });
 
+// delete items from the shopping list
+
+app.delete('/shopping-list/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted shopping list item ${req.params.id}`);
+  res.status(204).end();
+});
+
+// add a few recipes as examples
+
 Recipes.create('chocolate milk', ['cocoa', 'milk', 'suger']);
 Recipes.create('pancakes', ['flower', 'milk', 'water', 'eggs', 'milkyway']);
+
+// retrieve recipes from the list
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 });
+
+// add recipes to the list
 
 app.post('/recipes', jsonParser, (req, res)=> {
   const requiredFields = ['name', 'ingredients'];
@@ -52,6 +69,14 @@ app.post('/recipes', jsonParser, (req, res)=> {
 
   const item = Recipes.create(req.body.name, req.body.ingredients);
   return res.status(201).json(item);
+});
+
+// delete recipes
+
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe ${req.params.id}`);
+  res.status(204).end();
 });
 
 // start the process at an envirnoment or 8080 port
